3590c7d6309bf8247ae9b5633d483028f80d35f3
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-07 14:17:38 -0800

    running server thru index.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const colors = require('colors');
-const http = require('http');
-const server = http.createServer((req, res) => {
-response.writeHead(200, { 'Content-Type': 'text/plain' });
-response.write('<h1>Hello World</h1>');
-response.end();
-  
-});
-const PORT = 5000;
-server.listen(PORT, () => console.log(`server running on port ${PORT}`.yellow.bold));
+// const colors = require('colors');
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+// res.setHeader('Content-Type', 'text/plain');
+// res.write('Hello World-2');
+// res.end();
+// });
+// const PORT = 8888;
+// server.listen(PORT, () => console.log(`server running on port ${PORT}`.yellow.bold));
 
 
 
 
-// response.writeHead(200, { 'Content-Type': 'text/plain' });
-// response.write('Hello World');
-// response.end();
+ const http = require('http');
+const PORT = 8888;
+function start() {
+  function onRequest(req, res) {
+    console.log('request recieved');
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello World-3');
+    res.end(); 
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log(`server running on port ${PORT}`.yellow.bold);
+}
+exports.start = start;
+
