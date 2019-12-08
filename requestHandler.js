ba7204e6d55e3ed75df99dee2a8e50c0137359b6
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-07 19:05:48 -0800

    everything is broken

--- requestHandler.js
+++ requestHandler.js
@@ -1,16 +1,19 @@
 const exec = require('child_process').exec;
 function start(res) {
   console.log('request handler "start" was called'.cyan);
-  exec('ls -lah', function(error,stdout, stderr) {
-   res.setHeader('Content-Type', 'text/plain');
-   res.writeHead(stdout);
-   res.end();
+  exec('find/', {
+    timeout: 10000, maxBuffer: 20000*1024 },
+   function (error, stdout, stderr) {
+     res.setHeader('Content-Type', 'text/plain');
+     res.writeHead(stdout);
+     res.end();
   });
 }
 function upload(res) {
   console.log('reqest handler upload called'.red);
   res.setHeader('Content-Type', 'text/plain');
   res.writeHead('hello uppload');
+  res.end();
 }
 exports.start = start;
 exports.upload = upload;
