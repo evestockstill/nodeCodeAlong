551d2e501332993cf37358f9c5d07e2a6f039a9e
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-07 19:52:43 -0800

    added post

--- requestHandler.js
+++ requestHandler.js
@@ -1,6 +1,6 @@
-const exec = require('child_process').exec;
-  function start(response) {
-    console.log('request handler "start" was called'.cyan);
+  const querystring = require('querystring');
+  function start(response, postData) {
+    console.log(`request handler 'start' was called`.cyan);
     const body = '<html>' +
       '<head>' +
       '<meta http-equiv="Content-Type" content="text/html; ' +
@@ -9,17 +9,17 @@ const exec = require('child_process').exec;
       '<body>' +
       '<form action="/upload" method="post">' +
       '<textarea name="text" rows="20" cols="60"></textarea>' +
-      '<input type="submit" value="Submit text" />' +
+      '<input type= "submit" value="Submit text"/>' +
       '</form>' +
       '</body>' +
       '</html>';
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(body);
   }
-function upload(response) {
-  console.log('reqest handler upload called'.red);
+function upload(response, postData) {
+  console.log(`reqest handler upload called`.red);
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('hello uppload?');
+  response.write(`you have sent the text: + querystring.parse(postData).text`);
   response.end();
 }
 exports.start = start;
